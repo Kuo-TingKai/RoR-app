a9b29c3cedf8e573b24451c301a6d57485a4e206
Kuo Ting-Kai <nehsm30126@gmail.com>
2025-07-13 23:10:40 +0800

    solving build/run issue

--- config/application.rb
+++ config/application.rb
@@ -66,12 +66,5 @@ module EcommercePlatform
       /http:\/\/*/,
       /https:\/\/*/
     ]
-
-    # 設定 Sentry
-    Sentry.init do |config|
-      config.dsn = ENV['SENTRY_DSN']
-      config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-      config.traces_sample_rate = 0.5
-    end
   end
 end 
